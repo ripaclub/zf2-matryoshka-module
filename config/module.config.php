0a99281a3f5bbd50b9fc93974e338c213590f849
Leonardo Grasso <me@leonardograsso.com>
2014-04-04 15:44:04 +0200

    Added default resultset configuration

--- config/module.config.php
+++ config/module.config.php
@@ -1,10 +1,16 @@
 <?php
-
 return [
     'service_manager' => [
         'invokables' => [
             'Matryoshka\Model\ModelManager' => 'Matryoshka\Model\ModelManager',
+            'Matryoshka\Model\ResultSet\ArrayObjectResultSet' => 'Matryoshka\Model\ResultSet\ArrayObjectResultSet',
+            'Matryoshka\Model\ResultSet\HydratingResultSet' => 'Matryoshka\Model\ResultSet\HydratingResultSet',
         ],
+        'shared' => array(
+            'Matryoshka\Model\ModelManager' => true,
+            'Matryoshka\Model\ResultSet\ArrayObjectResultSet' => false,
+            'Matryoshka\Model\ResultSet\HydratingResultSet' => false,
+        ),
     ],
     'controller_plugins' => [
         'factories' => [
